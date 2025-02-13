6b1e1f45f8c1e9d2267df27ef7b4d5008639212f
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-13 17:44:02 -0500

    Task 2 - Employee Hourly Wage Calculation

--- cc_7.js
+++ cc_7.js
@@ -6,3 +6,12 @@ function calculateInvoice(subtotal, taxRate, discount) {
 // Test Data
 calculateInvoice(100, 0.08, 5); // Expected output: "Total Invoice: $103.00"
 calculateInvoice(500, 0.1, 20); // Expected output: "Total Invoice: $530.00"
+
+// Task2 - Function Expression
+const calculateHourlyWage = function(salary, hoursPerweek) {
+    let hourlyWage = salary / (hoursPerweek * 52);
+    console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`);
+}; // Declaring a function that calculates hourly wage.
+// Test Data
+calculateHourlyWage(52000, 40); // Expected output: "Hourly Wage: $25.00"
+calculateHourlyWage(75000, 35); // Expected output: "Hourly Wage: $41.21"
