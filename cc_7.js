75afcddd658abfe82559d130d1b9de5df829a478
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-13 18:03:04 -0500

    Task 3 - Customer Loyalty Discount

--- cc_7.js
+++ cc_7.js
@@ -15,3 +15,13 @@ const calculateHourlyWage = function(salary, hoursPerweek) {
 // Test Data
 calculateHourlyWage(52000, 40); // Expected output: "Hourly Wage: $25.00"
 calculateHourlyWage(75000, 35); // Expected output: "Hourly Wage: $41.21"
+
+// Task3 - Arrow Function
+const calculateLoyaltyDiscount = (amount, years) => {
+    let discount = years >= 5 ? 0.15 : years >= 3 ? 0.10 : 0.05; // Applying 15%, 10%, and 5% discount.
+    let discountedPrice = amount - (amount * discount);
+    console.log(`Disocunted Price: $${discountedPrice.toFixed(2)}`);
+}; // Writing an arrow function that applies a discount. 
+// Test Data
+calculateLoyaltyDiscount(100, 6); // Expected output: "Discounted Price: $85.00"
+calculateLoyaltyDiscount(200, 2); // Expected output: "Discounted Price: $190.00"
