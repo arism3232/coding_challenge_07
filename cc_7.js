1cf3d8b3f0fe0b906d9179a6562775d049fc38ea
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-16 20:32:28 -0500

    Fixed Task 2

--- cc_7.js
+++ cc_7.js
@@ -8,13 +8,13 @@ calculateInvoice(100, 0.08, 5); // Expected output: "Total Invoice: $103.00"
 calculateInvoice(500, 0.1, 20); // Expected output: "Total Invoice: $530.00"
 
 // Task2 - Function Expression
-const calculateHourlyWage = function(salary, hoursPerweek) {
-    let hourlyWage = salary / (hoursPerweek * 52);
-    console.log(`Hourly Wage: $${hourlyWage.toFixed(2)}`);
+function calculateHourlyWage(salary, hoursPerWeek) {
+    const hourlyWage = salary / (hoursPerWeek * 52);
+    return `Hourly Wage: $${hourlyWage.toFixed(2)}`
 }; // Declaring a function that calculates hourly wage.
 // Test Data
-calculateHourlyWage(52000, 40); // Expected output: "Hourly Wage: $25.00"
-calculateHourlyWage(75000, 35); // Expected output: "Hourly Wage: $41.21"
+console.log(calculateHourlyWage(52000, 40)); // Expected output: "Hourly Wage: $25.00"
+console.log(calculateHourlyWage(75000, 35)); // Expected output: "Hourly Wage: $41.21"
 
 // Task3 - Arrow Function
 const calculateLoyaltyDiscount = (amount, years) => {
